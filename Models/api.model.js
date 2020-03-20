345a8869fdee7dc9277ca9fdf2b1bcdde73cdcab
s-alex-smith <salexsmith91@gmail.com>
2020-03-20 10:48:37 +0000

    minor adjustments testing server

--- Models/api.model.js
+++ Models/api.model.js
@@ -2,6 +2,6 @@ const { readFile } = require("fs").promises;
 
 exports.selectEndpoints = () => {
   return readFile("./endpoints.json", "utf8").then(result => {
-    return JSON.parse(result);
+    return result;
   });
 };
